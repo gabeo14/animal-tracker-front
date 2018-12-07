172c6c57d33f52e7556aa67f45cc11ff77559a5a
Gabe Oleinik <gomusic14@gmail.com>
2018-12-07 15:43:36 -0500

    Timeline finally working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
-import * as React from 'react';
-import './App.css';
+import 'bulma-timeline/dist/css/bulma-timeline.min.css'
+import 'bulma/css/bulma.css'
+import * as React from 'react'
+import './App.scss'
 
-import logo from './logo.svg';
+import NavBar from './Pages/Components/NavBar'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <NavBar />
+        <div className="timeline">
+          <header className="timeline-header">
+            <span className="tag is-medium is-primary">Start</span>
+          </header>
+          <div className="timeline-item is-primary">
+            <div className="timeline-marker is-primary" />
+            <div className="timeline-content">
+              <p className="heading">January 2016</p>
+              <p>Timeline content - Can include any HTML element</p>
+            </div>
+          </div>
+          <div className="timeline-item is-warning">
+            <div className="timeline-marker is-warning is-image is-32x32">
+              <img src="http://bulma.io/images/placeholders/32x32.png" />
+            </div>
+            <div className="timeline-content">
+              <p className="heading">February 2016</p>
+              <p>Timeline content - Can include any HTML element</p>
+            </div>
+          </div>
+          <header className="timeline-header">
+            <span className="tag is-primary">2017</span>
+          </header>
+          <div className="timeline-item is-danger">
+            <div className="timeline-marker is-danger is-icon">
+              <i className="fa fa-flag" />
+            </div>
+            <div className="timeline-content">
+              <p className="heading">March 2017</p>
+              <p>Timeline content - Can include any HTML element</p>
+            </div>
+          </div>
+          <header className="timeline-header">
+            <span className="tag is-medium is-primary">End</span>
+          </header>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App

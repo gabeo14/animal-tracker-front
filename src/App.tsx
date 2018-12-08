0cee9d1c7e73af3d75dc05f6603ffc198e271ced
Gabe Oleinik <gomusic14@gmail.com>
2018-12-07 22:07:25 -0500

    List card layed out

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import 'bulma-checkradio/dist/css/bulma-checkradio.min.css'
 import 'bulma-timeline/dist/css/bulma-timeline.min.css'
 import 'bulma/css/bulma.css'
 import * as React from 'react'
@@ -18,33 +19,96 @@ class App extends React.Component {
             <div className="timeline-marker is-primary" />
             <div className="timeline-content">
               <p className="heading">January 2016</p>
-              <p>Timeline content - Can include any HTML element</p>
+              <div className="card">
+                <div className="card-content">
+                  <div className="content">
+                    <ul>
+                      <li>
+                        <h2 className="title is-2">Feed</h2>
+                        <ul>
+                          <li>
+                            <input type="checkbox" />
+                            Chomp - 1 Hopper Mouse
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Kirby - 1 Large Pinky Mouse
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Specter - 1 Large Pinky Mouse
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h2 className="title is-2">Clean</h2>
+                        <ul>
+                          <li>
+                            <input type="checkbox" />
+                            Daquiri
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Whistle
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Ember kitty
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h2 className="title is-2">Water</h2>
+                        <ul>
+                          <li>
+                            <input type="checkbox" />
+                            Zuko
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Kiki
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Charmy
+                          </li>
+                        </ul>
+                      </li>
+                      <li>
+                        <h2 className="title is-2">Misc.</h2>
+                        <ul>
+                          <li>
+                            <input type="checkbox" />
+                            Clean Atlas Filter
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Move Up Mike RoweBoat
+                          </li>
+                          <li>
+                            <input type="checkbox" />
+                            Clean Poop Sand
+                          </li>
+                        </ul>
+                      </li>
+                      {/* <div className="field">
+                      <input
+                        className="is-checkradio is-success"
+                        id="exampleCheckboxSuccess"
+                        type="checkbox"
+                        name="exampleCheckboxSuccess"
+                        checked="checked"
+                      />
+                      <label for="exampleCheckboxSuccess">
+                        Checkbox - success
+                      </label>
+                    </div> */}
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="timeline-item is-warning">
-            <div className="timeline-marker is-warning is-image is-32x32">
-              <img src="http://bulma.io/images/placeholders/32x32.png" />
-            </div>
-            <div className="timeline-content">
-              <p className="heading">February 2016</p>
-              <p>Timeline content - Can include any HTML element</p>
-            </div>
-          </div>
-          <header className="timeline-header">
-            <span className="tag is-primary">2017</span>
-          </header>
-          <div className="timeline-item is-danger">
-            <div className="timeline-marker is-danger is-icon">
-              <i className="fa fa-flag" />
-            </div>
-            <div className="timeline-content">
-              <p className="heading">March 2017</p>
-              <p>Timeline content - Can include any HTML element</p>
-            </div>
-          </div>
-          <header className="timeline-header">
-            <span className="tag is-medium is-primary">End</span>
-          </header>
         </div>
       </div>
     )
